0eb686ff94425d20c0a0f21857f7ddd270221df7
Jitendra447 <jitendradholpuria@poplify.com>
2016-03-19 11:53:57 +0530

    authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 	before_filter :configure_permitted_parameters, if: :devise_controller?  # for permitting parameters at time of sign_up.
- #	before_action :authenticate_author!, :except => [:show, :index, :author_profile]
+ 	before_action :authenticate_author!, :except => [:index]
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
